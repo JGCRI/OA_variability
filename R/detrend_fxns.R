a160ea448737a61bf94c35f21bdfd1b24ed94330
kdorheim <kalyn.dorheim@pnnl.gov>
2017-10-10 16:00:53 -0400

    Linear regression detrending

--- R/detrend_fxns.R
+++ R/detrend_fxns.R
@@ -1,130 +1,79 @@
 # ------------------------------------------------------------------------------
-# Purpose: This script contains the functions related to detrending time series
-# data for futher analysis.
+# Purpose: This script contains the functions related to de-trending time series
+# data for further analysis.
 #
 # Created by: Dorheim, Kalyn
-# Created on: xxx
+# Created on: October 10 2017
 # Modified:   xxx
 #
-# Notes:
+# Notes: Started with linear de-trending but will need to add polynomial methods...
 # ------------------------------------------------------------------------------
-# 1. detrend.linear_fit
+# internal.detrend
 # ------------------------------------------------------------------------------
-detrend.linear_fit <- function(df, make_grid = FALSE){
+#' For a single input detrend a time series based on the slope
+#'
+#' \code{internal.detrend} for a single model observation detrend the time series
+#' by the mean from a linear fit.
+#'
+#' @param df data frame to detrend
+#' @param return_type return the slope or the data
+#' @importFrom dplyr %>%
+#' @return a data frame containing the slope or detrended data
+#' @keywords internal
 
-  # Checks
-  #
-  # Column Check
-  check.column(df, "detrend.linear_fit input", c("model", "ensemble", "experiment", "time", "value", "variable", "units"))
+internal.detrend_linear <- function(df, return_type){
 
-  # Check for NAs
-  if(is.na(df$value)){
-    stop("Missing value in detrend.linear_fit input.")
+  # Find slope using a simple linear regression.
+  fit  <- lm(df$value ~ df$time)
+  pred <- sapply(df$time, FUN = function(time){fit$coefficients[1] + time * fit$coefficients[2]})
+  pred <- as.vector(pred)
+
+  df$value <- df$value - pred
+
+  if(grepl(pattern = "[s|s]lope", x = return_type)){
+    return(pred)
+  } else {
+    return(df)
   }
 
-  if(is.na(df$time)){
-    stop("Missing time in detrend.linear_fit input.")
-  }
+} # end of the internal detrend function
 
-  # Functional Code
-  #
-  # Firs make a list to store output in
-  output = list()
+# ------------------------------------------------------------------------------
+# detrend.linear
+# ------------------------------------------------------------------------------
+#' Linear regression detrending
+#'
+#' \code{detrend} for a single model observation detrend the time series
+#' by the mean from a linear fit.
+#'
+#' @param df data frame containing the different observations to detrend.
+#' @importFrom dplyr %>%
+#' @return a data frame containing a list of all the detrended observations and slopes
 
-  # Save CMIP5 information
-  en <- unique(df$ensemble)
-  ex <- unique(df$experiment)
-  m  <- unique(df$model)
-  v  <- unique(df$variable)
-  u  <- unique(df$units)
+detrend.linear <- function(df){
 
-  # Do the linear regression
-  fit <- stats::lm(value~time, df)
+  # Check the columns
+  check.column(df, "df for detrend function", required_columns = c("value", "model", "experiment", "ensemble", "basin",
+                                                                   "variable", "time"))
 
-  # Get the summary statistics about the linear regression
-  # fit and format for output.
-  sum_fit  <- base::summary(fit)
-
-  tibble::as_tibble(sum_fit$coefficients) %>%
-    dplyr::bind_cols(coefficient = c("y-intercept", "slope")) %>%
-    dplyr::mutate(ensemble = en, experiment = ex, model = m, variable = v) ->
-    output[["coefficient_data"]]
-
-
-  # Figures
-
-  # My perfered figure settings
-  # My preferred figure settings
-  my_settings  <- ggplot2::theme(text = element_text(size = 13)) +
-    ggplot2::theme(axis.text.x = element_text(angle = 45, hjust = 1))
-
-  # Determine the slope and intercept values
-  slope_val <- fit$coefficients[2]
-  inter_val <- fit$coefficients[1]
-
-  # Determine the slope and intercept p values
-  slope_p <- output$coefficient_data$`Pr(>|t|)`[2]
-  inter_p <- output$coefficient_data$`Pr(>|t|)`[1]
-
-  # Raw Data vs Fit
-  #
+  # Detrend the data
   df %>%
-    ggplot2::ggplot(aes(x = time, y = value)) +
-    ggplot2::geom_line(size = 1.5) +
-    ggplot2::geom_abline(slope = slope_val, intercept = inter_val, size = 1.5, color = 2) +
-    my_settings +
-    ggplot2::labs(y = paste0(v,"  ",u), title = paste0("Data vs. Fit\n", m," ",ex, " ",en),
-                  caption = paste0("slope = ", signif(slope_val,3), "(",signif(slope_p,3),")\nintercept = ",signif(inter_val,3), "(",signif(inter_p,3),")")) ->
-    output[["figures"]][["data_v_fit"]]
+    group_by(ensemble, experiment, model, variable, basin) %>%
+    do(value = internal.detrend(., return_type = "data")) %>%
+    tidyr::unnest() ->
+    data
 
-  # Residual vs. fitted value
-  #
-  cbind(residuals = sum_fit$residuals, fitted = fit$fitted.values) %>%
-   as.data.frame %>%
-   ggplot2::ggplot(aes(x = fitted, y = residuals)) +
-   ggplot2::geom_point(size = 1.5) +
-   ggplot2::geom_hline(yintercept = 0, size = 1.5, color = 2) +
-   my_settings +
-   ggplot2::labs(y = "Residuals", x = "Fitted Values", title = paste0("Residuals vs Fitted\n", m," ",v," ",ex, " ",en),
-                 caption = paste0("slope = ", signif(slope_val,3), "(",signif(slope_p,3),")\nintercept = ",signif(inter_val,3), "(",signif(inter_p,3),")")) ->
-   output[["figures"]][["residuals_v_fitted"]]
+  # Save the slope
+  df %>%
+    group_by(ensemble, experiment, model, variable, basin) %>%
+    do(value = internal.detrend_linear(., return_type = "slope")) %>%
+    tidyr::unnest() ->
+    slope
 
- # Histogram of the Residuals
- #
- ggplot2::qplot(x = sum_fit$residuals, geom = "histogram", bins = 20) +
-   my_settings +
-   ggplot2::labs(y = "Frequency", x = "Residual", title = paste0("Histogram of Residuals\n", m," ",v," ",ex, " ",en),
-                 caption = paste0("slope = ", signif(slope_val,3), "(",signif(slope_p,3),")\nintercept = ",signif(inter_val,3), "(",signif(inter_p,3),")")) ->
-   output[["figures"]][["residual_histogram"]]
+  # Return the output
+  return(list(slope = slope, data = data))
 
- # Normal QQ plot
- #
- ggplot2::ggplot(df) +
-   ggplot2::stat_qq(aes(sample = sum_fit$residuals)) +
-   my_settings +
-   ggplot2::labs(y = "Residual", x = "Theoretical Quantiles", title = paste0("Normal Q-Q\n", m," ",v," ",ex, " ",en),
-                 caption = paste0("slope = ", signif(slope_val,3), "(",signif(slope_p),")\nintercept = ",signif(inter_val,3), "(",signif(inter_p,3),")")) ->
-   output[["figures"]][["normal_qq"]]
-
- if(make_grid == TRUE) {
-
-   # Save all of the figures in a gird for easish printing, this will increase the running time.
-   output[["figures"]][["data_v_fit"]] -> p1
-   output[["figures"]][["residuals_v_fitted"]] + ggplot2::labs(caption = "") -> p2
-   output[["figures"]][["residual_histogram"]] + ggplot2::labs(caption = "") -> p3
-   output[["figures"]][["normal_qq"]] + ggplot2::labs(caption = "") -> p4
-
-   gridExtra::grid.arrange(p1, p2, p3, p4) ->
-     output[["figures"]][["grid"]]
-
- } # make grid if statement
-
-
- # Return
- return(output)
-
-} # end of the function
-
+} # end of detrend
 
 # ----
 # End
