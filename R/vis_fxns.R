6980262ad973c6bb0449118615bf209ec4bf356e
kdorheim <kalyn.dorheim@pnnl.gov>
2017-11-03 16:43:37 -0400

    Added vis.monthly_mean function and MY_SETTINGS constant for ggplot themes

--- R/vis_fxns.R
+++ R/vis_fxns.R
@@ -1,4 +1,4 @@
-# ------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------------------
 # Purpose: This script contains the vis family of functions. All of these
 # functions are related to plotting/graphing cmip5 data in some way. This script
 # contains the following functions: vis.is_outlier(), vis.exploratory(),
@@ -9,7 +9,12 @@
 # Modified:   xxx
 #
 # Notes: vis.time_series need a better way to lable the time axis.
-# ------------------------------------------------------------------------------
+
+# Define my fav figure settings
+MY_SETTINGS <- ggplot2::theme(text = element_text(size = 13)) +
+  ggplot2::theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+# -------------------------------------------------------------------------------------------
 # vis.is_outlier()
 # ------------------------------------------------------------------------------
 #' Identify outliers
@@ -118,13 +123,13 @@ vis.add_fig_name <- function(fig_list){
 
 # To do i need to figure out a better way to label the x axis
 
-internal.time_series <- function(input_df){
+internal.time_series <- function(input_df, subtitle = ""){
 
   # Checks
   #
   # Check the data frame for the required columns
   req_cols <- c("time", "units", "value", "model",  "experiment", "variable",
-                "ensemble", "month", "year", "method", "basin")
+                "ensemble", "method", "basin")
   check.column(input_df, "to_plot", req_cols)
 
   df <- input_df
@@ -150,12 +155,13 @@ internal.time_series <- function(input_df){
     my_settings +
     facet_wrap(facets = "basin", ncol = 3, scales = "free") +
     labs(y = paste0(var," ", units), title = "Time Series",
-         caption = paste0("Historical and future experiments plotted together for aesthetic purposes only"))
+         caption = paste0("Historical and future experiments plotted together for aesthetic purposes only"),
+         subtitle = subtitle)
 
 } # end of the vis.time_series
 
 
-# ------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------------------
 # plot.time_series
 # ------------------------------------------------------------------------------
 #' Genreate time series plots for the fld weighted means directly from pic
@@ -172,7 +178,7 @@ internal.time_series <- function(input_df){
 #' @return a data frame with all of the meta information
 #' @keywords internal
 
-plot.time_series <- function(data){
+plot.time_series <- function(data, subtitle = ""){
 
   out = list()
   var_list <- unique(data$variable)
@@ -182,15 +188,15 @@ plot.time_series <- function(data){
     to_plot   <- dplyr::filter(data, variable == var_list[i])
     variable  <- unique(to_plot$variable)
 
-    out[[paste0(variable)]] <- internal.time_series(to_plot)
+    out[[paste0(variable)]] <- internal.time_series(to_plot, subtitle)
 
   }
 
   return(out)
 }
 
-# ------------------------------------------------------------------------------
-# vis.amplitude()
+# -------------------------------------------------------------------------------------------
+# vis.amplitude() - FLAG FOR REMOVAL
 # ------------------------------------------------------------------------------
 #' Line plots for a monthly amplitude data frame
 #'
@@ -244,8 +250,113 @@ vis.amplitude <- function(input){
   return(out)
 
 } # end of the vis.amplitude function
+# -------------------------------------------------------------------------------------------
+# vis.monthly_mean()
+# -------------------------------------------------------------------------------------------
+#' Line plots for monthly mean values
+#'
+#' \code{vis.monthly_mean} visualize the monthly mean values with error bars
+#'
+#' @param data a summary stats data frame with desired grouping information for figures
+#' @import ggplot2
+#' @importFrom dplyr %>%
+#' @return a list of figures of all of the monthly line plots
+
+vis.monthly_mean <- function(data){
+
+  # First format the input data frame into something we can print. Format the input data set
+  data <- tidyr::spread(data, value_type, value)
+
+  # Define the internal function for creating the year labels
+  stats.year_labels <- function(data){
+
+    data %>%
+      dplyr::select(experiment, start_year, end_year) %>%
+      dplyr::distinct() %>%
+      dplyr::mutate(label = paste0(experiment, " : ", start_year, " - ", end_year)) %>%
+      dplyr::select(label)
+
+  } # end of internal function
+
+
+  # Save the model name label.
+  model_label <- unique(data$model)
+  if(length(model_label) > 1){model_label <- "All models"}
+
+  # Create the list where to store the outputs.
+  out = list()
+
+  # Save the variable list for the for loop
+  var_list <- unique(data$variable)
+
+  for(i in 1:length(var_list)){
+
+    to_plot   <- filter(data, variable == var_list[i])
+    yr_labels <- stats.year_labels(to_plot)
+    variable  <- unique(to_plot$variable)
+
+    to_plot %>%
+      ggplot(aes(x = month, y = mean, color = experiment)) +
+      geom_line(size = 1.2) +
+      geom_ribbon(aes(ymin = mean - 2*sd, ymax = mean + 2*sd, x = month, group = experiment, linetype=NA), alpha = 0.25) +
+      facet_wrap(facets = "basin", ncol = 3, scale = "free") +
+      theme(text = element_text(size = 13)) +
+      theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+      labs(title = paste0(model_label , " mean monthly with 2 sigma band"),
+           y = paste0("Mean Detrended ", variable),
+           caption = paste0(yr_labels[1,], "\n", yr_labels[2,])) +
+      scale_x_discrete(limits = to_plot$month, labels = to_plot$month_name) ->
+      out[[paste0(variable)]]
+
+  } # end of variable for loop
+
+  # Return output
+  return(out)
+}
 
 
 
 
+# -------------------------------------------------------------------------------------------
+# vis.amplitude_density()
+# -------------------------------------------------------------------------------------------
+
+vis.amplitude_density <- function(data){
+
+  # Create an empty list to save the different variable figures in
+  out = list()
+
+  # Save the model name label.
+  model_label <- unique(data$model)
+  if(length(model_label) > 1){model_label <- "All models"}
+
+  # Save the variable list for the variable for loop.
+  var_list <- unique(data$variable)
+
+  # Variable plots for loop, that plots each variable individually.
+  for(i in 1:length(var_list)){
+
+    to_plot   <- dplyr::filter(data, variable == var_list[i])
+    variable  <- unique(to_plot$variable)
+
+    to_plot %>%
+      ggplot(aes(x = amplitude, fill = experiment)) +
+      geom_density(alpha=.5, na.rm = TRUE) +
+      facet_wrap(facets = "basin", ncol = 3, scales = "free") +
+      theme(text = element_text(size = 13)) +
+      theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+      labs(x = "Seasonal Amplitude",
+           y = "Density",
+           title = paste0(model_label, " ", variable, " Amplitude Distrubution Plot")) ->
+      out[[paste0(variable)]]
+
+  } # end of plotting for loop
+
+  # Return the list of figures
+  return(out)
+
+}
+
+
+# -----
 # End
