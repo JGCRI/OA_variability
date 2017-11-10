c47c2b6c90d2328ebfbfbeac6e893ab2d690b9d9
kdorheim <kalyn.dorheim@pnnl.gov>
2017-11-10 17:03:26 -0500

    Optional print out figures as pdfs

---
+++ exec/figure_scripts/visualize_IPSL.R
@@ -0,0 +1,162 @@
+# ------------------------------------------------------------------------------
+# Purpose: This script looks at the observations for the complete data set for
+# IPSL only.
+#
+# Created by: Dorheim, Kalyn
+# Created on: October 11 2017
+# Modified:   xxx
+#
+# Notes:
+# ------------------------------------------------------------------------------
+# Environment
+# ------------------------------------------------------------------------------
+library(dplyr); devtools::load_all(); library(ggplot2)
+
+# ------------------------------------------------------------------------------
+# Script Functions
+# ------------------------------------------------------------------------------
+# I think this will be usefull will probably move it to the vis_fxns script
+stats.year_labels <- function(data){
+
+  data %>%
+    dplyr::select(experiment, start_year, end_year) %>%
+    dplyr::distinct() %>%
+    dplyr::mutate(label = paste0(experiment, " : ", start_year, " - ", end_year)) %>%
+    dplyr::select(label)
+
+}
+
+
+# ------------------------------------------------------------------------------
+# Get Data Sets
+# ------------------------------------------------------------------------------
+# Get the raw, trended or undtrended data from
+raw_path <- list.files("data", "basin_mean", full.names = TRUE)
+get(load(raw_path)) %>%
+  filter(grepl(x = model, pattern = "IPSL")) ->
+  raw_data
+
+# Get the detrended .rda object for the all models
+detrended_path <- list.files("data", "detrended_data", full.names = TRUE)
+get(load(detrended_path)) %>%
+  filter(grepl(x = model, pattern = "IPSL")) ->
+  detrended_data
+
+# Get the summary statistics .rda object for all models
+summary_path <- list.files("data", "summary_stats", full.names = TRUE)
+get(load(summary_path)) %>%
+  filter(grepl(x = model, pattern = "IPSL")) ->
+  summary_data
+
+# Get the amplitude values .rda object for all models
+amp_path <- list.files("data", "amplitude", full.names = TRUE)
+get(load(amp_path)) %>%
+  filter(grepl(x = model, pattern = "IPSL-CM5A-MR")) ->
+  amplitude_data
+
+
+# ------------------------------------------------------------------------------
+# Figures Code
+# ------------------------------------------------------------------------------
+# Time Series Plots
+# ------------------------------------------------------------------------------
+# Raw and detrended data
+raw <- plot.time_series(raw_data)
+detrended_figs <- plot.time_series(detrended_data)
+
+
+# ------------------------------------------------------------------------------
+# Monthly Means
+# ------------------------------------------------------------------------------
+data <- tidyr::spread(summary_data, value_type, value)
+mean_monthly = list()
+
+var_list <- unique(data$variable)
+for(i in 1:length(var_list)){
+
+  to_plot   <- filter(data, variable == var_list[i])
+  yr_labels <- stats.year_labels(to_plot)
+  variable  <- unique(to_plot$variable)
+
+  to_plot %>%
+    ggplot(aes(x = month, y = mean, color = experiment)) +
+    geom_line(size = 1.2) +
+    geom_ribbon(aes(ymin = mean - 2*sd, ymax = mean + 2*sd, x = month, group = experiment, linetype=NA), alpha = 0.25) +
+    facet_wrap(facets = "basin", ncol = 3, scale = "free") +
+    theme(text = element_text(size = 13)) +
+    theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+    labs(title = "IPSL-CM5A-MR mean monthly with 2 sigma band",
+         y = paste0("Mean Detrended ", variable),
+         caption = paste0(yr_labels[1,], "\n", yr_labels[2,])) +
+    scale_x_discrete(limits = to_plot$month, labels = to_plot$month_name) ->
+    mean_monthly[[paste0(variable)]]
+
+} # end of plotting for loop
+
+
+# ------------------------------------------------------------------------------
+# Amplitude Time Series
+# ------------------------------------------------------------------------------
+amplitude = list()
+var_list  <- unique(amplitude_data$variable)
+
+
+for(i in 1:length(var_list)){
+
+  to_plot   <- dplyr::filter(amplitude_data, variable == var_list[i])
+  variable  <- unique(to_plot$variable)
+
+  to_plot %>%
+    ggplot(aes(x = year, y = amplitude, color = experiment)) +
+    geom_line(size = 1.2) +
+    facet_wrap(facets = "basin", ncol = 3, scales = "free" ) +
+    theme(text = element_text(size = 13)) +
+    theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+    labs(x = "Year",
+         y = "Seasonal Amplitude",
+         title = paste0("IPSL-CM5A-MR ", variable, " Amplitude Time Series")) ->
+    amplitude[[paste0(variable)]]
+
+} # end of plotting for loop
+
+
+# ------------------------------------------------------------------------------
+# CESM Amplitude Distribution
+# ------------------------------------------------------------------------------
+amplitude_distribution = list()
+var_list  <- unique(amplitude_data$variable)
+
+
+for(i in 1:length(var_list)){
+
+  to_plot   <- dplyr::filter(amplitude_data, variable == var_list[i])
+  variable  <- unique(to_plot$variable)
+
+  to_plot %>%
+    ggplot(aes(amplitude, fill = experiment)) +
+    #geom_histogram(bins = 40, alpha=.5, position="identity") +
+    geom_density(alpha=.5) +
+    facet_wrap(facets = "basin", ncol = 3, scales = "free") +
+    theme(text = element_text(size = 13)) +
+    theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+    labs(x = "Seasonal Amplitude",
+         y = "Density",
+         title = paste0("IPSL-CM5A-MR ", variable, " Amplitude Distrubution Plots")) ->
+    amplitude_distribution[[paste0(variable)]]
+
+} # end of plotting for loop
+
+
+# ------------------------------------------------------------------------------
+# Save IPSL Plots
+# ------------------------------------------------------------------------------
+# Combine the time series plots into a list
+time_series = list(detrended = detrended_figs, raw = raw, amplitude = amplitude)
+
+# Save all of the figures in a single list
+FIGS.IPSL = list(time_series = time_series, mean_monthly = mean_monthly,
+                  distribution = amplitude_distribution)
+save(FIGS.IPSL, file = "figs/markdownFIGS_IPSL.rda")
+
+# -----
+# End
