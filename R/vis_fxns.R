5d7faeaae53bf390608b02f4d36ed48449044876
kdorheim <kalyn.dorheim@pnnl.gov>
2017-11-17 15:42:12 -0500

    Added a MY_SETTINGS constant to the figures function scipt, this will allow for change to ggplto text size to be applied to all figures

--- R/vis_fxns.R
+++ R/vis_fxns.R
@@ -11,7 +11,7 @@
 # Notes: vis.time_series need a better way to lable the time axis.
 
 # Define my fav figure settings
-MY_SETTINGS <- ggplot2::theme(text = ggplot2::element_text(size = 13)) +
+MY_SETTINGS <- ggplot2::theme(text = ggplot2::element_text(size = 20)) +
   ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 45, hjust = 1))
 
 # -------------------------------------------------------------------------------------------
@@ -44,13 +44,6 @@ vis.is_outlier <- function(x){
 
 vis.compare_id <- function(df){
 
-  # Set up
-  #
-  # My preferred figure settings
-  my_settings  <- ggplot2::theme(text = element_text(size = 13)) +
-    ggplot2::theme(axis.text.x = element_text(angle = 45, hjust = 1))
-
-
   # Drop any levels
   df <- droplevels(df)
 
@@ -80,7 +73,7 @@ vis.compare_id <- function(df){
     ggplot2::ggplot(aes(x = time, y = value, fill = id, color = id)) +
     ggplot2::geom_line(size = 1.5) +
     ggplot2::labs(y = paste0(var," ", uni)) +
-    my_settings +
+    MY_SETTINGS +
     ggplot2::labs(caption = paste0(exp, "\n", en),
                   title = paste0("Compare some id\n add more info")) ->
     fig; fig
@@ -109,6 +102,39 @@ vis.add_fig_name <- function(fig_list){
 
 } # end of vis.add_fig_name
 
+# -------------------------------------------------------------------------------------------
+# vis.model_label
+# -------------------------------------------------------------------------------------------
+#' Generate a model label based on data frame contents
+#'
+#' \code{vis.model_label} This function saves a string to be used as a model label based on
+#' the contents of the input data frame. Most often it is used to reduce code in complicated
+#' visual functions.
+#'
+#' @param data a data frame containing a model column to summarise
+#' @return a string containing info about the model(s) in the figure
+
+vis.model_label <- function(data){
+
+  # Check to make sure the required input is present
+  check.column(data, "model_label() input", "model")
+
+  model_list <- unique(data$model)
+
+  if(length(model_list) == 1){
+
+    return_label <- model_list
+
+  } else {
+
+    return_label <- "Observations from Multiple Models"
+
+  }
+
+  return(return_label)
+
+} # end of vis.model_label
+
 
 # -------------------------------------------------------------------------------------------
 # internal.time_series
@@ -128,7 +154,7 @@ internal.time_series <- function(input_df, subtitle = ""){
   # Checks
   #
   # Check the data frame for the required columns
-  req_cols <- c("time", "units", "value", "model",  "experiment", "variable",
+  req_cols <- c("date", "units", "value", "model",  "experiment", "variable",
                 "ensemble", "method", "basin")
   check.column(input_df, "to_plot", req_cols)
 
@@ -139,24 +165,19 @@ internal.time_series <- function(input_df, subtitle = ""){
   # Define the empty list to save the figures in.
   out_list = list()
 
-  # Define my fav figure settings
-  my_settings <- ggplot2::theme(text = element_text(size = 13)) +
-    ggplot2::theme(axis.text.x = element_text(angle = 45, hjust = 1))
-
-
   var     <- unique(df$variable)
   units   <- unique(df$units)
   en      <- unique(df$ensemble)
   method  <- unique(df$method)
 
   df %>%
-    ggplot(aes(x = time, y = value, group = model, color = model)) +
-    geom_line(size = 1.2) +
-    my_settings +
+    ggplot(aes(x = date, y = value, group = model, color = model)) +
+    geom_line(size = 2) +
     facet_wrap(facets = "basin", ncol = 3, scales = "free") +
     labs(y = paste0(var," ", units), title = "Time Series",
          caption = paste0("Historical and future experiments plotted together for aesthetic purposes only"),
-         subtitle = subtitle)
+         subtitle = subtitle) +
+    MY_SETTINGS
 
 } # end of the vis.time_series
 
@@ -238,6 +259,7 @@ vis.amplitude <- function(input){
       theme(text = element_text(size = 13)) +
       theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
       theme(legend.position = "none") +
+      MY_SETTINGS +
       scale_x_discrete(limits = month_name_df$month, labels = month_name_df$month_name) +
       labs(y = paste0(type, " ", info$variable, " ", info$units),
            x = "Month",
@@ -343,8 +365,7 @@ vis.amplitude_density <- function(data){
       ggplot(aes(x = amplitude, fill = experiment)) +
       geom_density(alpha=.5, na.rm = TRUE) +
       facet_wrap(facets = "basin", ncol = 3, scales = "free") +
-      theme(text = element_text(size = 13)) +
-      theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+      MY_SETTINGS +
       labs(x = "Seasonal Amplitude",
            y = "Density",
            title = paste0(model_label, " ", variable, " Amplitude Distrubution Plot")) ->
@@ -356,6 +377,180 @@ vis.amplitude_density <- function(data){
   return(out)
 
 }
+
+# -------------------------------------------------------------------------------------------
+# internal.KS_scatterplot
+# -------------------------------------------------------------------------------------------
+#' Generate a series of scatter plots looking at different K and S comparisons
+#'
+#' \code{internal.KS_scatterplot} This function creates scatter plots for the K and S
+#' measurelments and comparing by variable and basin combinations
+#'
+#' @param data a data frame containing the wide set K and S stats to plot
+#' @return a list containing the various scatter plots
+
+internal.KS_scatterplot <- function(data){
+
+  # Save info for plot labels
+  model_label <- vis.model_label(data)
+  stat_label  <- unique(data$stat_variable)
+
+  # Scatter plot of historical vs future K or S colored
+  # by variable
+  data %>%
+    ggplot(aes(historical, future, color = variable)) +
+    geom_point(size = 6) +
+    MY_SETTINGS +
+    labs(x = paste0(stat_label, " historical"),
+         y = paste0(stat_label, " future"),
+         title = paste0(model_label, "\nComparing Variables")) ->
+    p1
+
+  # Facet the variable K or S scatter plot by basin
+  p1 +
+    facet_wrap("basin", ncol = 3) +
+    labs(subtitle = "By Basin") ->
+    p2
+
+  # Scatter plot of the hisotircal vs future K or S colored by
+  # basin
+  data %>%
+    ggplot(aes(historical, future, color = basin)) +
+    geom_point(size = 6) +
+    MY_SETTINGS +
+    labs(x = paste0(stat_label, " historical"),
+         y = paste0(stat_label, " future"),
+         title = paste0(model_label, "\nComparing Basins")) ->
+    p3
+
+  # Facet the colored by basin scatter plot by variable
+  p3 +
+    facet_wrap("variable", ncol = 2) +
+    labs(subtitle = "By Variable") ->
+    p4
+
+
+  # If you are looking at an individual model then the color by model graphs will
+  # not really matter then this if statement sets the figure = NULL.
+  if(length(unique(data$model)) > 1) {
+
+    data %>%
+      ggplot(aes(historical, future, color = model)) +
+      geom_point(size = 6) +
+      MY_SETTINGS +
+      labs(x = paste0(stat_label, " historical"),
+           y = paste0(stat_label, " future"),
+           title = paste0(model_label, "\nComparing Models")) ->
+      p5
+
+    # Facet the colored by model by both variable and basin
+    p5 +
+      facet_wrap("variable", ncol = 2) +
+      labs(subtitle = "By Variable") ->
+      p6
+
+    p5 +
+      facet_wrap("basin", ncol = 3) +
+      labs(subtitle = "By Basin") ->
+      p7
+
+  } else {
+
+    # If you are looking at an individual model then this
+    p5 <- NULL
+    p6 <- NULL
+    p7 <- NULL
+
+  }
+
+
+  # Save all of the scatter plot figures in a list with relevant names.
+  out = list(variable = p1, variable_by_basin = p2, basin = p3, basin_variable = p4,
+             model = p5, model_by_variable = p6, model_by_basin = p7)
+
+  return(out)
+} # end of the internal.KS_scatterplot
+
+
+# -------------------------------------------------------------------------------------------
+# vis.KS_scatter_plots
+# -------------------------------------------------------------------------------------------
+#' Generate a series of scatter plots looking at different K and S comparisons
+#'
+#' \code{vis.KS_scatter_plots} This function creates scatter plots for the K and S
+#' measurelments, only for the K/S historical vs future scatter plots
+#'
+#' @param data data frame of the K and S amplitude distribution measurements
+#' @return a list containing the various scatter plots
+
+
+vis.KS_scatter_plots <- function(data){
+
+  # Start by fomrating the data into the wide format so that
+  # we can plot the different experiments on the sides.
+  data %>%
+    # Rename the future experiment scenario as future so that the function is compatible
+    # with other rcp scenarios.
+    mutate(experiment = if_else(experiment != "historical", "future", experiment)) %>%
+    tidyr::spread(experiment, value) ->
+    to_plot
+
+  # Get the K and S scatter plots then name and save figures in a
+  # flat list.
+  to_plot %>%
+    group_by(stat_variable) %>%
+    do(fig = internal.KS_scatterplot(.)) ->
+    out
+
+  # Format output
+  KS_scatter <- setNames(object = out$fig, nm = out$stat_variable)
+
+  return(KS_scatter)
+
+} # end of vis.KS_plots
+
+
+# -------------------------------------------------------------------------------------------
+# vis.delta_KS
+# -------------------------------------------------------------------------------------------
+#' Make the scatter plot of delta K and S
+#'
+#' \code{vis.delta_KS} This function makes the a scatter plot from change in K and S from the
+#' future vs historical data sets.
+#'
+#' @param data data frame of the cahnge in K and S amplitude distribution measurements
+#' @parm print_outliers default is set to false, if set to true then will label observations that count as outliers
+#' @return a single scatter plot of the change in K and S values
+
+vis.delta_KS <- function(data, print_outliers = FALSE){
+
+  # Save a label of info about the models used
+  mo_label <- vis.model_label(data)
+
+  # The scatter/box plot
+  data %>%
+    group_by(stat_variable, basin, variable) %>%
+    mutate(outlier = ifelse(vis.is_outlier(delta), model, NA)) %>%
+    ungroup ->
+    to_plot
+
+  to_plot %>%
+    ggplot(aes(basin, delta, shape = stat_variable, color = stat_variable)) +
+    geom_point(size = 6) +
+    MY_SETTINGS +
+    facet_wrap("variable") +
+    labs(title = paste0(mo_label, "\nDelta K and S comparison"),
+         y = "Change in K and S") ->
+    p
+
+  # Add the labels to the figures if specified
+  if(print_outliers){
+   p <- p + geom_text(data = to_plot, aes(label = outlier), na.rm = TRUE, size = 3, check_overlap = TRUE)
+  }
+
+  return(p)
+
+} # end of the vis.delta_KS
 
 
 # -----
